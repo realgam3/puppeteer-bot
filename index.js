3503c1fbcc4f3fa6d9e4f4c48935cccd0923052e
Tomer Zait (realgam3) <realgam3@gmail.com>
2023-06-21 09:48:57 +0300

    fix rabbitmq env var name

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const bot = require("./bot");
 const amqplib = require("amqplib");
 const config = require(process.env.CONFIG_PATH || "./config.js");
 
-const RABITMQ_HOST = process.env.RABITMQ_HOST || config.queue.host;
-const RABITMQ_PORT = parseInt(process.env.RABITMQ_PORT || config.queue.port);
-const RABITMQ_USERNAME = process.env.RABITMQ_USERNAME || config.queue.password;
-const RABITMQ_PASSWORD = process.env.RABITMQ_PASSWORD || config.queue.username;
+const RABBITMQ_HOST = process.env.RABBITMQ_HOST || config.queue.host;
+const RABBITMQ_PORT = parseInt(process.env.RABBITMQ_PORT || config.queue.port);
+const RABBITMQ_USERNAME = process.env.RABBITMQ_USERNAME || config.queue.password;
+const RABBITMQ_PASSWORD = process.env.RABBITMQ_PASSWORD || config.queue.username;
 const QUEUE_NAME = process.env.QUEUE_NAME || config.queue.name;
 
 function sleep(time = 1000) {
@@ -25,10 +25,10 @@ function sleep(time = 1000) {
             connection = await amqplib.connect({
                 maxLength: 1,
                 protocol: "amqp",
-                port: RABITMQ_PORT,
-                hostname: RABITMQ_HOST,
-                username: RABITMQ_USERNAME,
-                password: RABITMQ_PASSWORD,
+                port: RABBITMQ_PORT,
+                hostname: RABBITMQ_HOST,
+                username: RABBITMQ_USERNAME,
+                password: RABBITMQ_PASSWORD,
             });
         } catch (error) {
             console.error(error);
