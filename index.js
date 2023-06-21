ccd227eced79d95af9687c97a64b5e656535c3d5
Tomer Zait (realgam3) <realgam3@gmail.com>
2023-06-21 11:59:23 +0300

    add utils as module

--- index.js
+++ index.js
@@ -1,22 +1,12 @@
 #!/usr/bin/env node
 
-const bot = require("./bot");
 const amqplib = require("amqplib");
+
+const bot = require("./bot");
+const utils = require("./utils");
+
 const config = require(process.env.CONFIG_PATH || "./config.js");
 
-const RABBITMQ_HOST = process.env.RABBITMQ_HOST || config.queue.host;
-const RABBITMQ_PORT = parseInt(process.env.RABBITMQ_PORT || config.queue.port);
-const RABBITMQ_USERNAME = process.env.RABBITMQ_USERNAME || config.queue.password;
-const RABBITMQ_PASSWORD = process.env.RABBITMQ_PASSWORD || config.queue.username;
-const QUEUE_NAME = process.env.QUEUE_NAME || config.queue.name;
-
-function sleep(time = 1000) {
-    return new Promise(resolve => {
-        setTimeout(() => {
-            return resolve();
-        }, time);
-    });
-}
 
 (async () => {
     let connection = null;
@@ -25,23 +15,23 @@ function sleep(time = 1000) {
             connection = await amqplib.connect({
                 maxLength: 1,
                 protocol: "amqp",
-                port: RABBITMQ_PORT,
-                hostname: RABBITMQ_HOST,
-                username: RABBITMQ_USERNAME,
-                password: RABBITMQ_PASSWORD,
+                port: config.queue.port,
+                hostname: config.queue.host,
+                username: config.queue.username,
+                password: config.queue.password,
             });
         } catch (error) {
             console.error(error);
-            await sleep();
+            await utils.sleep();
         }
     }
 
     const channel = await connection.createChannel();
-    await channel.assertQueue(QUEUE_NAME, {
+    await channel.assertQueue(config.queue.name, {
         durable: false
     });
     await channel.prefetch(1);
-    await channel.consume(QUEUE_NAME, async function (msg) {
+    await channel.consume(config.queue.name, async function (msg) {
         console.debug("\n[x] Received: %s", msg.content.toString());
         try {
             const data = JSON.parse(msg.content.toString());
---
+++ utils.js
@@ -0,0 +1,8 @@
+function sleep(ms = 1000) {
+    return new Promise(r => setTimeout(r, ms));
+}
+
+
+module.exports = {
+    sleep,
+}
